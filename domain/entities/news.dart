94043b2260f16e8769a25af2b3bf8a0d9dd3a00d
themik <themek93@gmail.com>
2021-06-01 08:44:48 +0200

    News extends Equtable

--- domain/entities/news.dart
+++ domain/entities/news.dart
@@ -1,20 +1,16 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
-import '../repsonse.dart';
-
-class News extends Response {
+class News extends Equatable {
   final int newsId;
   final String newsTitle;
   final String newsEntryDate;
   final String newsThumbnail;
 
   News({
-    @required status,
-    @required currentPage,
-    @required lastPage,
     @required this.newsId,
     @required this.newsTitle,
     @required this.newsEntryDate,
     @required this.newsThumbnail,
-  }) : super(status, currentPage, lastPage);
+  });
 }
