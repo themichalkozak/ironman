968288feae973bf32179aecf5c145e0a7c47fa82
themik <themek93@gmail.com>
2021-06-01 11:40:10 +0200

    created detail response for news

---
+++ domain/entities/news/news_detailed_response.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:ironman/domain/entities/news/news.dart';
+
+class NewsDetailedResponse extends News {
+
+  final String author;
+  final String body;
+
+  NewsDetailedResponse({
+    @required newsId,
+    @required newsTitle,
+    @required newsEntryDate,
+    @required newsThumbnail,
+    @required this.author,
+    @required this.body,
+  }):super(newsId: newsId,newsTitle: newsTitle,newsEntryDate: newsEntryDate,newsThumbnail: newsThumbnail);
+
+  @override
+  List get props => [author,body];
+}
