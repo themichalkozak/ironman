b119cba35aac3906c128388bc7a7368c325e2eda
themik <themek93@gmail.com>
2021-06-02 15:17:26 +0200

    Update eventDetail entity

--- domain/event/entity/event_detail.dart
+++ domain/event/entity/event_detail.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:ironman/domain/event/entity/event.dart';
 
-class EventDetailResponse extends Event {
-
+class EventDetail extends Event {
   final List<EventSpecification> eventSpecifications;
   final String eventWebSite;
   final String information;
 
-  EventDetailResponse({
+  EventDetail({
+    @required int eventId,
+    @required String eventTitle,
+    @required String eventDate,
+    @required String eventFinishDate,
+    @required String eventVenue,
+    @required String eventCountryName,
+    @required String eventFlag,
     @required this.eventSpecifications,
     @required this.eventWebSite,
     @required this.information,
-  });
+  }) : super(
+            eventId: eventId,
+            eventTitle: eventTitle,
+            eventDate: eventDate,
+            eventFinishDate: eventFinishDate,
+            eventVenue: eventVenue,
+            eventCountryName: eventCountryName,
+            eventFlag: eventCountryName);
 }
 
 class EventSpecification {
