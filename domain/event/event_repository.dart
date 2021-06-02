03a6e0ea4484cb86b254c180276587131a28e4e6
themik <themek93@gmail.com>
2021-06-02 09:45:33 +0200

    Change return type to List<Event>

--- domain/event/event_repository.dart
+++ domain/event/event_repository.dart
@@ -1,14 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:ironman/core/failure.dart';
-import 'package:ironman/domain/event/entity/event_detailed_response.dart';
-import 'package:ironman/domain/event/entity/event_list_response.dart';
+import 'package:ironman/domain/event/entity/event_detail.dart';
+import 'package:ironman/domain/event/entity/event.dart';
 import 'package:ironman/domain/event/event_tense.dart';
 
 abstract class EventRepository {
-  Future<Either<Failure, EventListingResponse>> getEvents(
+  Future<Either<Failure, List<Event>>> getEvents(
       EventTense eventTense);
 
-  Future<Either<Failure, EventListingResponse>> getEventsByQuery(
+  Future<Either<Failure, Event>> getEventsByQuery(
       String query, EventTense eventTense);
 
   Future<Either<Failure, EventDetailResponse>> getEventById(int id);
