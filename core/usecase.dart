ccf8748e67ea5d5fcc461c42b93cc5fd313c7a35
themik <themek93@gmail.com>
2021-06-02 10:51:49 +0200

    created generic usecase class

---
+++ core/usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:ironman/core/failure.dart';
+
+abstract class UseCase<Type,Params> {
+
+  Future<Either<Failure,Type>> call(Params params);
+
+}
+
+class NoParams extends Equatable {}
