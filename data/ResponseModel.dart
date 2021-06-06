3778139e84c342456276854b8b224b05123aaa94
themik <themek93@gmail.com>
2021-06-06 09:29:30 +0200

    Added message params

--- data/ResponseModel.dart
+++ data/ResponseModel.dart
@@ -6,20 +6,23 @@ class ResponseModel extends Response {
   final int currentPage;
   final int lastPage;
   final List data;
+  final String message;
 
   ResponseModel({
     @required this.status,
     @required this.currentPage,
     @required this.lastPage,
     @required this.data,
+    @required this.message,
   });
 
-  factory ResponseModel.fromJson(Map<String,dynamic> json){
+  factory ResponseModel.fromJson(Map<String,dynamic> json) {
     return ResponseModel(
       status: json['status'],
       currentPage: json['current_page'],
       lastPage: json['last_page'],
-      data: json['data'] as List
+      data: json['data'] as List,
+      message: json['message']
     );
   }
 }
