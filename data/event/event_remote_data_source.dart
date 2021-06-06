7ba1337bc97154f01d35d389255d457d9afc5fa4
themik <themek93@gmail.com>
2021-06-06 09:29:47 +0200

    Implement get Events in remote data source

--- data/event/event_remote_data_source.dart
+++ data/event/event_remote_data_source.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:ironman/core/constants.dart';
+import 'package:ironman/core/error/exceptions.dart';
+import 'package:ironman/core/error/failure.dart';
+import 'package:ironman/data/ResponseModel.dart';
 
 import 'package:ironman/data/event/EventDetailModel.dart';
 import 'package:ironman/data/event/EventModel.dart';
@@ -8,8 +13,8 @@ import 'package:ironman/domain/event/event_tense.dart';
 abstract class EventRemoteDataSource {
   Future<List<EventModel>> getEvents(EventTense eventTense);
 
-  Future<List<EventModel>> searchEventsByQuery(
-      String query, EventTense eventTense);
+  Future<List<EventModel>> searchEventsByQuery(String query,
+      EventTense eventTense);
 
   Future<EventDetailModel> getEventById(int id);
 }
@@ -20,17 +25,38 @@ class EventRemoteDataSourceImpl extends EventRemoteDataSource {
   EventRemoteDataSourceImpl(this.client);
 
   @override
-  Future<List<EventModel>> getEvents(EventTense eventTense) {
-    client.get(BASE_URL + '/events',
+  Future<List<EventModel>> getEvents(EventTense eventTense) async {
+    final response = await client.get(BASE_URL + '/events',
         headers: {'Content-Type': 'application/json', 'apikey': API_KEY});
+
+    if (response.statusCode != 200) {
+      throw ServerExceptions(message: 'Error');
+    }
+    final responseModel = ResponseModel.fromJson(json.decode(response.body));
+
+    if (responseModel.status == 'fail') {
+      throw ServerExceptions(message: responseModel.message);
+    }
+
+    List<EventModel> events = [];
+
+    if(responseModel.data == null){
+      throw NoElementFailure();
+    }
+
+    responseModel.data.forEach((element) {
+      events.add(EventModel.fromJson(element));
+    });
+
+    return events;
   }
 
   @override
   Future<EventDetailModel> getEventById(int id) {}
 
   @override
-  Future<List<EventModel>> searchEventsByQuery(
-      String query, EventTense eventTense) {
+  Future<List<EventModel>> searchEventsByQuery(String query,
+      EventTense eventTense) {
 //
   }
 }
