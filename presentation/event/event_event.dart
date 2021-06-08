dc1e55fa9c47aa465163cfd56ec7b762d19c7fb3
themik <themek93@gmail.com>
2021-06-08 11:02:37 +0200

    Created events for event_bloc

--- presentation/event/event_event.dart
+++ presentation/event/event_event.dart
@@ -1,4 +1,14 @@
 part of 'event_bloc.dart';
 
 @immutable
-abstract class EventEvent {}
+abstract class EventEvent extends Equatable {
+  EventEvent([List props = const <dynamic>[]]): super(props);
+}
+
+class EventInitial extends EventEvent {}
+
+class GetEvents extends EventEvent {
+  final EventTense eventTense;
+
+  GetEvents(this.eventTense) : super([eventTense]);
+}
