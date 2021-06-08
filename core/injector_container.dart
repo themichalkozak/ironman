82e245665bf420118943c0b4393bef7f149a33cf
themik <themek93@gmail.com>
2021-06-08 21:16:41 +0200

    Created file for get_it dependency

---
+++ core/injector_container.dart
@@ -0,0 +1,32 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ironman/core/platform/network_info.dart';
+import 'package:ironman/data/event/event_remote_data_source.dart';
+import 'package:ironman/data/event/event_repository_impl.dart';
+import 'package:ironman/domain/event/event_repository.dart';
+import 'package:ironman/domain/event/useCases/get_events.dart';
+import 'package:http/http.dart' as http;
+import 'package:ironman/presentation/event/bloc/bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Features
+  // UseCases
+  sl.registerLazySingleton(() => GetEvents(sl()));
+  // Bloc
+  sl.registerFactory<EventBloc>(() => (EventBloc(getEvents: sl())));
+  // Repositories
+  sl.registerLazySingleton<EventRepository>(
+      () => EventRepositoryImpl(
+          remoteDataSource: sl(),
+          networkInfo: sl()
+      ));
+  // Data sources
+  sl.registerLazySingleton<EventRemoteDataSource>(() => EventRemoteDataSourceImpl(sl()));
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  // External
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
