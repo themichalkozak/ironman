d09c4d7b89b6b9f60d99f27eb5806749b4e901fc
themik <themek93@gmail.com>
2021-06-08 12:41:06 +0200

    Add error const string

--- presentation/event/event_bloc.dart
+++ presentation/event/event_bloc.dart
@@ -2,14 +2,27 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:ironman/domain/event/entity/event.dart';
 import 'package:ironman/domain/event/event_tense.dart';
 import 'package:meta/meta.dart';
 
 part 'event_event.dart';
 part 'event_state.dart';
 
+const String NO_ELEMENT_FAILURE_MESSAGE = 'Event not found';
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+
 class EventBloc extends Bloc<EventEvent, EventState> {
-  EventBloc() : super(EventInitial());
+  final GetEvents getEvents;
+
+  EventBloc({
+    @required GetEvents getEvents,
+  }): assert(getEvents != null),
+  getEvents = getEvents;
+
+
+  @override
+  EventState get initialState => Empty();
 
   @override
   Stream<EventState> mapEventToState(
