64607bcc7263f5e4f418b5216b5280e0fe945a75
themik <themek93@gmail.com>
2021-06-12 06:47:11 +0200

    Detect bottom listView

--- lib/features/event/presentation/widgets/event_display.dart
+++ lib/features/event/presentation/widgets/event_display.dart
@@ -2,20 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:ironman/features/event/domain/entity/event.dart';
 import 'package:ironman/features/event/presentation/widgets/event_display_list_item.dart';
 
-class EventDisplay extends StatelessWidget {
+class EventDisplay extends StatefulWidget {
   final List<Event> events;
 
-  const EventDisplay({
+  EventDisplay({
     Key key,
     @required this.events,
   })  : assert(events != null),
         super(key: key);
 
   @override
+  _EventDisplayState createState() => _EventDisplayState();
+}
+
+class _EventDisplayState extends State<EventDisplay> {
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    setUpScrollControllerListener();
+    super.initState();
+  }
+
+  void setUpScrollControllerListener() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        print('NEXT PAGE');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      controller: _scrollController,
       itemBuilder: (context, index) {
-        return EventListItem(event: events[index]);
-      },itemCount: events.length,);
+        return EventListItem(event: widget.events[index]);
+      },
+      itemCount: widget.events.length,
+    );
   }
 }
