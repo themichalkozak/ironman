41b747f4564210f314626f48b3a69d10a1a69e9e
themik <themek93@gmail.com>
2021-06-15 19:14:37 +0200

    Added refresh bottom indicator

--- lib/features/event/presentation/widgets/event_display.dart
+++ lib/features/event/presentation/widgets/event_display.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ironman/features/event/domain/entity/event.dart';
-import 'package:ironman/features/event/presentation/bloc/bloc.dart';
 import 'package:ironman/features/event/presentation/widgets/event_display_list_item.dart';
 
 class EventDisplay extends StatefulWidget {
@@ -22,17 +20,7 @@ class _EventDisplayState extends State<EventDisplay> {
 
   @override
   void initState() {
-    setUpScrollControllerListener();
     super.initState();
-  }
-
-  void setUpScrollControllerListener() {
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        BlocProvider.of<EventBloc>(context).add(GetEventsEvent());
-      }
-    });
   }
 
   @override
@@ -43,12 +31,29 @@ class _EventDisplayState extends State<EventDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      controller: _scrollController,
-      itemBuilder: (context, index) {
-        return EventListItem(event: widget.events[index]);
-      },
-      itemCount: widget.events.length,
-    );
+    return NotificationListener(
+        onNotification: _handleScrollNotification,
+        child: ListView.builder(
+          controller: _scrollController,
+          itemBuilder: (context, index) {
+            return index == widget.events.length
+                ? Container(
+              alignment: Alignment.center,
+              height: 50,
+              color: Theme.of(context).primaryColor.withOpacity(0.7),
+                child: Text('Load more events..',style: TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold),))
+                : EventListItem(event: widget.events[index]);
+          },
+          itemCount: widget.events.length + 1,
+        ));
+  }
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    print(_scrollController.position.extentAfter);
+    if (notification is ScrollEndNotification &&
+        _scrollController.position.extentAfter == 0) {
+    //  todo not implemented
+    }
+    return false;
   }
 }
