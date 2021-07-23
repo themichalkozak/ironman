b63e719cf6f950d76e514cdb8588919da9e6a718
themik <themek93@gmail.com>
2021-07-23 11:19:47 +0200

    Added eventTense as param in Loaded EventState

--- lib/features/event/presentation/bloc/event_state.dart
+++ lib/features/event/presentation/bloc/event_state.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:ironman/features/event/domain/entity/event_detail.dart';
+import 'package:ironman/features/event/domain/event_tense.dart';
 
 @immutable
 abstract class EventState extends Equatable {
@@ -14,15 +15,17 @@ class Loading extends EventState {}
 class Loaded extends EventState {
   final List events;
   final bool isExhausted;
+  final EventTense eventTense;
 
   Loaded({
     @required this.events,
-    @required this.isExhausted
-  }): super([events,isExhausted]);
+    @required this.isExhausted,
+    @required this.eventTense
+  }): super([events,isExhausted,eventTense]);
 
 
   @override
-  String toString() => 'Loaded | events.length: ${events.length} | isExhausted: $isExhausted';
+  String toString() => 'Loaded | events.length: ${events.length} | isExhausted: $isExhausted | eventTense" $eventTense';
 }
 
 class LoadedDetail extends EventState {
